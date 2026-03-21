e237999dc768d80d0373c1c70715af0fe29f0e40
Kishan Kewal <iamdevildeveloper@gmail.com>
2026-03-21 20:54:19 +0530

    Testing branch

--- test.php
+++ test.php
@@ -1,12 +1,4 @@
 <?php
 
-echo "Running CI test...\n";
-$a = 5;
-$b = 5;
-
-if ($a + $b === 10) {
-    echo "✅ Test Passed\n";
-} else {
-    echo "❌ Test Failed\n";
-    exit(1);
-}
+echo "❌ Test Failed\n";
+exit(1);
