543b27666ed43c6798b41eb7828243fdb6f9ced2
Kishan Kewal <iamdevildeveloper@gmail.com>
2026-03-21 21:02:15 +0530

    Failing test

--- test.php
+++ test.php
@@ -1,12 +1,3 @@
 <?php
-
-echo "Running CI test...\n";
-$a = 5;
-$b = 5;
-
-if ($a + $b === 10) {
-    echo "✅ Test Passed\n";
-} else {
-    echo "❌ Test Failed\n";
-    exit(1);
-}
+echo "Test Failure";
+exit(1);
