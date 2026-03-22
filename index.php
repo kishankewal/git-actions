e71c3d297b9c2225e27763d9426b3c9c35b15f41
Kishan Kewal <iamdevildeveloper@gmail.com>
2026-03-22 10:14:07 +0530

    Pushing updated code

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo "Hello, World! from Kishan Kewal";
-echo "This is a test file for GitHub Actions CI/CD pipeline.";
+echo "Hello, World! from Kishan Kewal \n";
+echo "This is a test file for GitHub Actions CI/CD pipeline.\n";
+echo "This file will be used to demonstrate the CI/CD workflow with Docker and GitHub Actions.\n";
